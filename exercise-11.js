2b5774b151db193b507c53d7fb68460eec782421
Chrisna Aing <chrisna@chrisna.org>
2026-03-24 23:11:53 -0400

    Update dependencies.

--- exercise-11.js
+++ exercise-11.js
@@ -1,6 +1,6 @@
-const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:8099')
-const stream = WebSocket.createWebSocketStream(ws)
+const WebSocket = require("ws");
+const ws = new WebSocket("ws://localhost:8099");
+const stream = WebSocket.createWebSocketStream(ws);
 
 stream.pipe(process.stdout);
 stream.end("hello\n");
